fafbe4abc5d05f7d70a12e11b043e8a10c3b38c8
Vladimir Shcherbakov <vladimir@atomiclabs.com>
2008-06-29 08:17:59 +0000

    Copyright and licensing info added

--- src/HTTPMessageParser.cpp
+++ src/HTTPMessageParser.cpp
@@ -1,3 +1,11 @@
+// ------------------------------------------------------------------
+// pion-net: a C++ framework for building lightweight HTTP interfaces
+// ------------------------------------------------------------------
+// Copyright (C) 2007-2008 Atomic Labs, Inc.  (http://www.atomiclabs.com)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See http://www.boost.org/LICENSE_1_0.txt
+//
 #include <pion/net/HTTPMessageParser.hpp>
 
 using namespace pion::net;
