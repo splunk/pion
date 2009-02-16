f6bfb75e9b8b798bcffdfb5b6ef011d7b538a627
Mignon Belongie <mignon@atomiclabs.com>
2009-02-16 17:51:50 +0000

    Refactored unit test framework in two ways: 1) there are now global fixtures (PionUnitTestsConfig, PionCommonUnitTestsConfig, PionNetUnitTestsConfig, etc.) which contain a startup and teardown that run before the first and after the last unit test, and 2) there is a new utility class PionUnitTest with static helper functions like check_files_match(). The global fixtures can handle command line arguments: PionUnitTestsConfig uses '-v' to enable logging of messages from pion and PionPlatformUnitTestsConfig and PionEnterpriseUnitTestsConfig use '-v' to enable logging of messages from libxml2.

--- common/include/pion/PionUnitTestDefs.hpp
+++ common/include/pion/PionUnitTestDefs.hpp
@@ -10,8 +10,11 @@
 #ifndef __PION_PIONUNITTESTDEFS_HEADER__
 #define __PION_PIONUNITTESTDEFS_HEADER__
 
+#include <iostream>
+#include <fstream>
 #include <boost/test/unit_test.hpp>
 #include <boost/test/test_case_template.hpp>
+#include <pion/PionLogger.hpp>
 
 #ifdef _MSC_VER
 	#include <direct.h>
@@ -24,6 +27,119 @@
 #endif
 
 #define DIRECTORY_MAX_SIZE 1000
+
+
+struct PionUnitTest {
+	// This is passed to xmlSetGenericErrorFunc() to make libxml do nothing when an error
+	// occurs, rather than its default behavior of writing a message to stderr.
+	static void doNothing(void* ctx, const char* msg, ...) {
+	}
+
+	static void skip_comment_lines(std::ifstream& fs) {
+		int c;
+		while (!fs.eof() && fs.peek() == '#') {
+			while (! fs.eof()) {
+				c = fs.get();
+				if (c == '\n') {
+					if (fs.peek() == '\r')
+						fs.get();
+					break;
+				} else if (c == '\r') {
+					if (fs.peek() == '\n')
+						fs.get();
+					break;
+				}
+			}
+		}
+	}
+
+	static char* trim(char* str) {
+		for (unsigned len = strlen(str) - 1; len > 0; len--) {
+			if (str[len] == '\n' || str[len] == '\r')
+				str[len] = '\0';
+			else
+				break;
+		}
+		return str;
+	}
+
+	// Check for file match, use std::list for sorting the files, which will allow
+	// random order matching...
+	static bool check_files_match(const std::string& fileA, const std::string& fileB) {
+		// open files
+		std::ifstream a_file(fileA.c_str(), std::ios::in | std::ios::binary);
+		BOOST_REQUIRE(a_file.is_open());
+
+		std::ifstream b_file(fileB.c_str(), std::ios::in | std::ios::binary);
+		BOOST_REQUIRE(b_file.is_open());
+
+		// skip lines that start with #
+		skip_comment_lines(a_file);
+		skip_comment_lines(b_file);
+
+		// read and compare data in files
+		static const unsigned int BUF_SIZE = 4096;
+		char buf[BUF_SIZE];
+		std::list<std::string> a_lines, b_lines;
+
+		while (a_file.getline(buf, BUF_SIZE)) {
+			a_lines.push_back(trim(buf));
+			if (! b_file.getline(buf, BUF_SIZE))
+				return false;
+			b_lines.push_back(trim(buf));
+		}
+		if (b_file.getline(buf, BUF_SIZE))
+			return false;
+		if (a_file.gcount() != b_file.gcount())
+			return false;
+		a_lines.sort();
+		b_lines.sort();
+
+		if (a_lines != b_lines)
+			return false;
+
+		a_file.close();
+		b_file.close();
+
+		// files match
+		return true;
+	}
+};
+
+
+// PionUnitTestsConfig is intended for use as a global fixture.  By including the 
+// following line in one source code file of a unit test project, the constructor will
+// run once before the first test and the destructor will run once after the last test:
+
+// BOOST_GLOBAL_FIXTURE(PionUnitTestsConfig);
+
+struct PionUnitTestsConfig {
+	PionUnitTestsConfig() {
+		std::cout << "global setup for all pion unit tests\n";
+
+		// argc and argv do not include parameters handled by the boost unit test framework, such as --log_level.
+		int argc = boost::unit_test::framework::master_test_suite().argc;
+		char** argv = boost::unit_test::framework::master_test_suite().argv;
+
+		bool verbose = false;
+		if (argc > 1) {
+			if (argv[1][0] == '-' && argv[1][1] == 'v') {
+				verbose = true;
+			}
+		}
+		PION_LOG_CONFIG_BASIC;
+		pion::PionLogger log_ptr = PION_GET_LOGGER("pion");
+		if (verbose) {
+			PION_LOG_SETLEVEL_WARN(log_ptr);
+		} else {
+			std::cout << "Use '-v' to enable logging of errors and warnings from pion.\n";
+			PION_LOG_SETLEVEL_FATAL(log_ptr);
+		}
+	}
+	~PionUnitTestsConfig() {
+		std::cout << "global teardown for all pion unit tests\n";
+	}
+};
 
 
 /*
--- common/tests/PionCommonUnitTests.cpp
+++ common/tests/PionCommonUnitTests.cpp
@@ -7,7 +7,30 @@
 // See http://www.boost.org/LICENSE_1_0.txt
 //
 
+#include <iostream>
 #include <pion/PionConfig.hpp>
 
 #define BOOST_TEST_MODULE pion-common-unit-tests
 #include <boost/test/unit_test.hpp>
+
+#include <pion/PionUnitTestDefs.hpp>
+
+struct PionCommonUnitTestsConfig {
+	PionCommonUnitTestsConfig() {
+		std::cout << "global setup specific to pion-common\n";
+
+		// argc and argv do not include parameters handled by the boost unit test framework, such as --log_level.
+		int argc = boost::unit_test::framework::master_test_suite().argc;
+		char** argv = boost::unit_test::framework::master_test_suite().argv;
+
+		std::cout << "argc = " << argc << std::endl;
+		for (int i = 0; i < argc; ++i)
+			std::cout << "argv[" << i << "] = " << argv[i] << std::endl;
+	}
+	~PionCommonUnitTestsConfig() {
+		std::cout << "global teardown specific to pion-common\n";
+	}
+};
+
+BOOST_GLOBAL_FIXTURE(PionUnitTestsConfig);
+BOOST_GLOBAL_FIXTURE(PionCommonUnitTestsConfig);
