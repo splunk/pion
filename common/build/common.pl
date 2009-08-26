18db2048ab8eba2ce7e0814aac64922d1fc7f1c6
Michael Dickey <mike@atomiclabs.com>
2009-08-26 22:46:55 +0000

    Updated copyDirWithoutDotFiles() Perl function in common.pl to so that it now supports a third optional argument of a hash of name=value template parameters
    Added make_config.pl script which can now be used to generate a Pion
    configuration directory.  It takes two arguments where the first is the
    source directory (platform/build/config) and the second is the destination
    or output directory (platform/config).  Zero or more additional "NAME=VALUE"
    arguments may be specified to override the template parameters used to 
    generate the configuration files.

--- common/build/common.pl
+++ common/build/common.pl
@@ -14,6 +14,7 @@ use File::Copy;
 sub copyDirWithoutDotFiles(@) {
 	my $src_dir = shift();
 	my $dst_dir = shift();
+	my %templates = @_;
 	
 	# make sure directory exists
 	mkpath($dst_dir);
@@ -29,9 +30,24 @@ sub copyDirWithoutDotFiles(@) {
 			my $src_file = File::Spec->catfile($src_dir, $_);
 			my $dst_file = File::Spec->catfile($dst_dir, $_);
 			if (-d $src_file) {
-				copyDirWithoutDotFiles($src_file, $dst_file);
-			} else {
+				copyDirWithoutDotFiles($src_file, $dst_file, %templates);
+			} elsif ( scalar keys %templates == 0 ) {
+				# no templates specified -> just copy the file as-is
 				copy($src_file, $dst_file);
+			} else {
+				# template parameters were specified
+				# process key->value parameters while copying the file
+				# WARNING: this probably only works for text files
+				open(FROMFILE, $src_file) or die "could not open source template ($src_file)";
+				open(TOFILE, ">", $dst_file) or die "could not open $dst_file for writing";
+				while ( <FROMFILE> ) {
+					while ( ($key, $value) = each %templates ) {
+						s,\@$key\@,$value,;
+					}
+					print TOFILE $_;
+				}
+				close(FROMFILE);
+				close(TOFILE);
 			}
 		}
 	}
---
+++ common/build/make_config.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+# --------------------------------------------------
+# pion-platform configuration directory build script
+# --------------------------------------------------
+
+use File::Spec;
+use File::Path;
+use File::Copy;
+use File::Glob ':glob';
+
+# include perl source with common subroutines
+require File::Spec->catfile( ("common", "build"), "common.pl");
+
+
+# -----------------------------------
+# process argv & set global variables
+# -----------------------------------
+
+# check command line parameters
+die("usage: make_config.pl <SRCDIR> <DESTDIR> [<KEY>=<VALUE>]+") if ($#ARGV < 1);
+
+# set some global variables
+$SRCDIR = $ARGV[0];
+$DESTDIR = $ARGV[1];
+
+
+# ------------
+# main process
+# ------------
+
+# check source directory
+die "Source directory does not exist: $SRCDIR" if (! -d $SRCDIR);
+
+# build template parameter hash (start with default values)
+my %templates = ("PION_PLUGINS_DIRECTORY" => "../../platform/codecs/.libs/</PluginPath><PluginPath>../../platform/databases/.libs/</PluginPath><PluginPath>../../platform/reactors/.libs</PluginPath><PluginPath>../../platform/protocols/.libs/</PluginPath><PluginPath>../../platform/services/.libs/</PluginPath><PluginPath>../../net/services/.libs/",
+	"PION_DATA_DIRECTORY" => ".",
+	"PION_UI_DIRECTORY" => "../../platform/ui",
+	"PION_LOG_CONFIG" => "logconfig.txt");
+
+# update template parameter hash using values provided
+for ($n = 2; $n <= $#ARGV; ++$n) {
+	die "Bad template argument: \"$ARGV[$n]\"" if (! ($ARGV[$n] =~ m/([^=]+)=(.*)/) );
+	$templates{$1} = $2;
+}
+
+# clear out old files and directories at destination
+@oldfiles = bsd_glob($DESTDIR . "*");
+foreach (@oldfiles) {
+	rmtree($_);
+}
+
+# copy files using templates
+copyDirWithoutDotFiles($SRCDIR, $DESTDIR, %templates);
+
+print "* Done building configuration directory ($SRCDIR => $DESTDIR)\n";
