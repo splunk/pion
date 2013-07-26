86f2f2aa34f4daaf2de50a7882307b1ed0e8ab15
Michael Dickey <mike@cloudmeter.com>
2013-07-26 13:25:01 -0700

    Added unit tests for algorithm::xml_encode()
    Updated ChangeLog & bumped version to 5.0.3 for RC

--- tests/algorithm_tests.cpp
+++ tests/algorithm_tests.cpp
@@ -26,6 +26,119 @@ BOOST_AUTO_TEST_CASE(testURLEncodingOfStringWithNegativeCharacter) {
     BOOST_CHECK_EQUAL(algorithm::url_encode(s), "%E2bcde");
 }
 
+BOOST_AUTO_TEST_CASE(checkXmlEncodeWithAlphanumericString) {
+    BOOST_CHECK_EQUAL("Freedom7", algorithm::xml_encode("Freedom7"));
+}
+
+BOOST_AUTO_TEST_CASE(checkXmlEncodeWithAmpersand) {
+    BOOST_CHECK_EQUAL("A&amp;P", algorithm::xml_encode("A&P"));
+}
+
+BOOST_AUTO_TEST_CASE(checkXmlEncodeWithVariousSpecialXmlCharacters) {
+    BOOST_CHECK_EQUAL("&quot;1&quot; &lt; &quot;2&quot; &amp;&amp; &apos;b&apos; &gt; &apos;a&apos;", algorithm::xml_encode("\"1\" < \"2\" && 'b' > 'a'"));
+}
+
+// UTF-8 replacement character
+const std::string RC = "\xEF\xBF\xBD";
+
+BOOST_AUTO_TEST_CASE(checkXmlEncodeWithControlCharacters) {
+    char cc_array_1[] = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F";
+    char cc_array_2[] = "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
+    std::string cc_str_1(cc_array_1, 16);
+    std::string cc_str_2(cc_array_2, 16);
+    std::string expected_output_1 = RC + RC + RC + RC + RC + RC + RC + RC + RC + "\x09\x0A" + RC + RC + "\x0D" + RC + RC;
+    std::string expected_output_2 = RC + RC + RC + RC + RC + RC + RC + RC + RC + RC + RC + RC + RC + RC + RC + RC;
+
+    BOOST_CHECK_EQUAL(expected_output_1, algorithm::xml_encode(cc_str_1));
+    BOOST_CHECK_EQUAL(expected_output_2, algorithm::xml_encode(cc_str_2));
+}
+
+BOOST_AUTO_TEST_CASE(checkXmlEncodeWithValidUtf8TwoByteSequences) {
+    const char UTF8_ENCODED_TEST_CHAR_ARRAY[] = {
+        (char)0xCE, (char)0xB1,             // UTF-8 encoding of U+03B1 (GREEK SMALL LETTER ALPHA)
+        0x3D,                               // '='
+        0x31,                               // '1'
+        0x20,                               // space
+        (char)0xCE, (char)0xB2,             // UTF-8 encoding of U+03B2 (GREEK SMALL LETTER BETA)
+        0x3D,                               // '='
+        0x32};                              // '2'
+    const std::string UTF8_ENCODED_TEST_STRING(UTF8_ENCODED_TEST_CHAR_ARRAY, sizeof(UTF8_ENCODED_TEST_CHAR_ARRAY));
+    BOOST_CHECK_EQUAL(UTF8_ENCODED_TEST_STRING, algorithm::xml_encode(UTF8_ENCODED_TEST_STRING));
+}
+
+BOOST_AUTO_TEST_CASE(checkXmlEncodeWithValidUtf8ThreeByteSequences) {
+    const char UTF8_ENCODED_TEST_CHAR_ARRAY[] = {
+        (char)0xE2, (char)0x82, (char)0xA4,     // UTF-8 encoding of U+20A4 (LIRA SIGN)
+        0x32,                                   // '2'
+        0x3D,                                   // '='
+        (char)0xE2, (char)0x82, (char)0xA8,     // UTF-8 encoding of U+20A8 (RUPEE SIGN)
+        0x32};                                  // '3'
+    const std::string UTF8_ENCODED_TEST_STRING(UTF8_ENCODED_TEST_CHAR_ARRAY, sizeof(UTF8_ENCODED_TEST_CHAR_ARRAY));
+    BOOST_CHECK_EQUAL(UTF8_ENCODED_TEST_STRING, algorithm::xml_encode(UTF8_ENCODED_TEST_STRING));
+}
+
+BOOST_AUTO_TEST_CASE(checkXmlEncodeWithValidUtf8FourByteSequences) {
+    char UTF8_ENCODED_TEST_CHAR_ARRAY[] = {
+        (char)0xF0, (char)0x90, (char)0x82, (char)0x88,     // UTF-8 encoding of U+10088 (LINEAR B IDEOGRAM B107F SHE-GOAT)
+        (char)0xE2, (char)0x82, (char)0xA8,                 // UTF-8 encoding of U+2260 (NOT EQUAL TO)
+        (char)0xF0, (char)0x90, (char)0x82, (char)0x89};    // UTF-8 encoding of U+10089 (LINEAR B IDEOGRAM B107M HE-GOAT)
+    const std::string UTF8_ENCODED_TEST_STRING(UTF8_ENCODED_TEST_CHAR_ARRAY, sizeof(UTF8_ENCODED_TEST_CHAR_ARRAY));
+    BOOST_CHECK_EQUAL(UTF8_ENCODED_TEST_STRING, algorithm::xml_encode(UTF8_ENCODED_TEST_STRING));
+}
+
+// Any isolated high byte (i.e. > x7F) is invalid, but they are invalid for a variety of reasons.
+BOOST_AUTO_TEST_CASE(checkXmlEncodeWithInvalidSingleHighByte) {
+    // These are invalid because the second byte is > x7F and is not allowed as the first byte of a UTF-8 multi-byte sequence.
+    BOOST_CHECK_EQUAL("=" + RC + "=", algorithm::xml_encode("=\x80="));
+    BOOST_CHECK_EQUAL("=" + RC + "=", algorithm::xml_encode("=\xBF="));
+    BOOST_CHECK_EQUAL("=" + RC + "=", algorithm::xml_encode("=\xC0="));
+    BOOST_CHECK_EQUAL("=" + RC + "=", algorithm::xml_encode("=\xC1="));
+    BOOST_CHECK_EQUAL("=" + RC + "=", algorithm::xml_encode("=\xF5="));
+    BOOST_CHECK_EQUAL("=" + RC + "=", algorithm::xml_encode("=\xFF="));
+
+    // These are invalid because the second byte is the first byte of a UTF-8 2-byte sequence, but isn't followed by a valid second byte.
+    BOOST_CHECK_EQUAL("=" + RC + "=", algorithm::xml_encode("=\xC2="));
+    BOOST_CHECK_EQUAL("=" + RC + "=", algorithm::xml_encode("=\xDF="));
+
+    // These are invalid because the second byte is the first byte of a UTF-8 3-byte sequence, but isn't followed by a valid second byte.
+    BOOST_CHECK_EQUAL("=" + RC + "=", algorithm::xml_encode("=\xE0="));
+    BOOST_CHECK_EQUAL("=" + RC + "=", algorithm::xml_encode("=\xEF="));
+
+    // These are invalid because the second byte is the first byte of a UTF-8 4-byte sequence, but isn't followed by a valid second byte.
+    BOOST_CHECK_EQUAL("=" + RC + "=", algorithm::xml_encode("=\xF0="));
+    BOOST_CHECK_EQUAL("=" + RC + "=", algorithm::xml_encode("=\xF4="));
+}
+
+BOOST_AUTO_TEST_CASE(checkXmlEncodeWithInvalidTwoHighByteSequence) {
+    // These are invalid because the second byte is the first byte of a UTF-8 2-byte sequence, but isn't followed by a valid second byte.
+    BOOST_CHECK_EQUAL("=" + RC + RC + "=", algorithm::xml_encode("=\xC2\xC0="));
+    BOOST_CHECK_EQUAL("=" + RC + RC + "=", algorithm::xml_encode("=\xDF\xFF="));
+
+    // These are invalid because bytes 2 & 3 are the first and second bytes of a UTF-8 3-byte sequence, but aren't followed by a valid third byte.
+    BOOST_CHECK_EQUAL("=" + RC + RC + "=", algorithm::xml_encode("=\xE0\x80="));
+    BOOST_CHECK_EQUAL("=" + RC + RC + "=", algorithm::xml_encode("=\xEF\xBF="));
+
+    // These are invalid because bytes 2 & 3 are the first and second bytes of a UTF-8 4-byte sequence, but aren't followed by a valid third byte.
+    BOOST_CHECK_EQUAL("=" + RC + RC + "=", algorithm::xml_encode("=\xF0\x80="));
+    BOOST_CHECK_EQUAL("=" + RC + RC + "=", algorithm::xml_encode("=\xF4\xBF="));
+}
+
+BOOST_AUTO_TEST_CASE(checkXmlEncodeWithInvalidThreeHighByteSequence) {
+    // These are invalid because bytes 2 & 3 are the first and second bytes of a UTF-8 3-byte sequence, but aren't followed by a valid third byte.
+    BOOST_CHECK_EQUAL("=" + RC + RC + RC + "=", algorithm::xml_encode("=\xE0\x80\xC0="));
+    BOOST_CHECK_EQUAL("=" + RC + RC + RC + "=", algorithm::xml_encode("=\xEF\xBF\xFF="));
+
+    // These are invalid because bytes 2 & 3 are the first and second bytes of a UTF-8 4-byte sequence, but aren't followed by a valid third byte.
+    BOOST_CHECK_EQUAL("=" + RC + RC + RC + "=", algorithm::xml_encode("=\xF0\x80\xC0="));
+    BOOST_CHECK_EQUAL("=" + RC + RC + RC + "=", algorithm::xml_encode("=\xF4\xBF\xFF="));
+}
+
+BOOST_AUTO_TEST_CASE(checkXmlEncodeWithInvalidFourHighByteSequence) {
+    // These are invalid because bytes 2-4  are the first, second and third bytes of a UTF-8 4-byte sequence, but aren't followed by a valid fourth byte.
+    BOOST_CHECK_EQUAL("=" + RC + RC + RC + RC + "=", algorithm::xml_encode("=\xF0\x80\x80\xC0="));
+    BOOST_CHECK_EQUAL("=" + RC + RC + RC + RC + "=", algorithm::xml_encode("=\xF4\xBF\xBF\xFF="));
+}
+
 BOOST_AUTO_TEST_CASE(testBase64Routines) {
     std::string original;
     std::string original_base64;
