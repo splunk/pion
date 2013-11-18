5a4fc85e7e44de746e976b32aed9cf1aed9c476f
Nikita Ofitserov <Nikita.Ofitserov@transas.com>
2013-11-18 16:46:10 +0400

    Use type-correct character literals

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -262,9 +262,9 @@ std::string algorithm::xml_encode(const std::string& str)
                 result += *(++ptr);
             } else {
                 // insert replacement char
-                result += 0xef;
-                result += 0xbf;
-                result += 0xbd;
+                result += '\xef';
+                result += '\xbf';
+                result += '\xbd';
             }
         } else if (*ptr >= 0xE0 && *ptr <= 0xEF) {
             // three-byte sequence
@@ -275,9 +275,9 @@ std::string algorithm::xml_encode(const std::string& str)
                 result += *(++ptr);
             } else {
                 // insert replacement char
-                result += 0xef;
-                result += 0xbf;
-                result += 0xbd;
+                result += '\xef';
+                result += '\xbf';
+                result += '\xbd';
             }
         } else if (*ptr >= 0xF0 && *ptr <= 0xF4) {
             // four-byte sequence
@@ -290,15 +290,15 @@ std::string algorithm::xml_encode(const std::string& str)
                 result += *(++ptr);
             } else {
                 // insert replacement char
-                result += 0xef;
-                result += 0xbf;
-                result += 0xbd;
+                result += '\xef';
+                result += '\xbf';
+                result += '\xbd';
             }
         } else {
             // insert replacement char
-            result += 0xef;
-            result += 0xbf;
-            result += 0xbd;
+            result += '\xef';
+            result += '\xbf';
+            result += '\xbd';
         }
         ++ptr;
     }
