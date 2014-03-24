ceae52bf85f7e5d7dc83f3a308fc2011779939cc
Michael Dickey <mdickey@splunk.com>
2014-03-24 14:13:43 -0700

    Bumped version to 5.0.6
    Updated ChangeLog for 5.0.6 release
    
    Updated copyrights and misc files for move from Cloudmeter to Splunk

--- cmake/PionUtils.cmake
+++ cmake/PionUtils.cmake
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------
 # pion:  a Boost C++ framework for building lightweight HTTP interfaces
 # ---------------------------------------------------------------------
-# Copyright (C) 2013 Cloudmeter, Inc.  (http://www.cloudmeter.com)
+# Copyright (C) 2007-2014 Splunk Inc.  (https://github.com/splunk/pion)
 #
 # Distributed under the Boost Software License, Version 1.0.
 # See http://www.boost.org/LICENSE_1_0.txt
